87fbbbfa1e9628a46b4fd39cc6ebce7f60a83a12
Lucas Esclapez <lesclapez@lbl.gov>
2021-02-03 13:30:42 -0800

    Update conversion of E/N to Td.

--- SourceCpp/Advance.cpp
+++ SourceCpp/Advance.cpp
@@ -116,7 +116,7 @@ PeleC::do_mol_advance(
   // }
 
   amrex::Real mwt[NUM_SPECIES];
-  EOS::molecular_weight(mwt);
+  EOS::molecular_weight(mwt);    // CGS
   amrex::Real NA = 6.0221409e23; // 1/mol
 
   // Calculate the reduced electric field strength
@@ -132,7 +132,7 @@ PeleC::do_mol_advance(
        gbox, [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept {
          amrex::Real ndens = 0.0;
          for(int n=0; n<NUM_SPECIES; n++) ndens += Sfab(i,j,k,UFS+n) * (1.0/mwt[n]) * NA;
-         redEfab(i,j,k) = std::sqrt( AMREX_D_TERM (Efab(i,j,k,0)*Efab(i,j,k,0), + Efab(i,j,k,1)*Efab(i,j,k,1), + Efab(i,j,k,2)*Efab(i,j,k,2))) / ndens * 1e17;
+         redEfab(i,j,k) = std::sqrt( AMREX_D_TERM (Efab(i,j,k,0)*Efab(i,j,k,0), + Efab(i,j,k,1)*Efab(i,j,k,1), + Efab(i,j,k,2)*Efab(i,j,k,2))) / ndens * 1e-7 * 1e17;   // Conversion erg/cm^2 -> V/cm^2 and V/cm^2 -> Td
        });
   }
 #endif
