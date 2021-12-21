b8140f0d63c333e7c7017ab5f45ad127d958b7cb
Nicholas Deak <ndeak@login1.frontera.tacc.utexas.edu>
2021-12-21 13:30:50 -0600

    bug fixes for dielectric relaxation time scale monitoring

--- Source/Advance.cpp
+++ Source/Advance.cpp
@@ -188,6 +188,9 @@ PeleC::do_mol_advance(
   }
   // VisMF::Write(Efield,"GradPhiCC");
 
+
+
+
   // Compute S^{n} = MOLRhs(U^{n})
   if (verbose) {
     amrex::Print() << "... Computing MOL source term at t^{n} " << std::endl;
@@ -197,6 +200,21 @@ PeleC::do_mol_advance(
   amrex::Real flux_factor = 0;
   getMOLSrcTerm(Sborder, molSrc, time, dt, flux_factor);
   if(ef_use_NLsolve) Sborder.setVal(0.0, UFS+E_ID, 1);
+
+  // Calculate the cel-centered dielectric relaxation timescales
+  for (amrex::MFIter mfi(KSpec_old, amrex::TilingIfNotGPU()); mfi.isValid(); ++mfi) {
+     const amrex::Box& tbox = mfi.tilebox();
+     const amrex::Box gbox = amrex::grow(tbox, numGrow());
+     const auto muEfab = KSpec_old.array(mfi,E_ID);
+     const auto ne_fab = Sborder.array(mfi,UFS+E_ID);
+     const auto diele_fab = dielectric_ts.array(mfi);
+     amrex::ParallelFor(
+       gbox, [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept {
+          amrex::Real ne_val = amrex::max(amrex::Math::abs(ne_fab(i,j,k))/EFConst::me_cgs, 1.0e-10);
+          amrex::Real mu_E = amrex::max(amrex::Math::abs(muEfab(i,j,k)), 1.0e-10);
+          diele_fab(i,j,k) = amrex::Math::abs(EFConst::eps0_cgs / (EFConst::elemCharge * mu_E * ne_val));
+       });
+  }
 
 #ifdef PELEC_USE_PLASMA
   if (ef_use_NLsolve) {
