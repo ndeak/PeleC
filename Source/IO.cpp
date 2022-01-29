8e8d47aadcb818df427ff9b199c6051969bb26cb
Nicholas Deak <ndeak@el4.ib0.cm.hpc.nrel.gov>
2022-01-29 15:22:20 -0700

    Fixes to dielectric relaxation time step limiting

--- Source/IO.cpp
+++ Source/IO.cpp
@@ -1232,7 +1232,8 @@ void PeleC::writeMonitorFile(amrex::MultiFab& S, const amrex::Real *mwt, amrex::
     amrex::Real max_Edrift = amrex::max(max_Edrift_x, max_Edrift_y, max_Edrift_z);
     amrex::Real Ddtodx2 = (max_De/min_rho) * dt / (dx[0] * dx[0]);
     amrex::Real CFL = max_Edrift * dt / dx[0];
-    amrex::Real min_dielectric = dielectric_ts.min(0, 0, false);
+    amrex::Real min_edrift = 0.3 * dx[0] / max_Edrift;
+    amrex::Real min_dielectric = 0.5*dielectric_ts.min(0, 0, false);
 
     if (amrex::ParallelDescriptor::IOProcessor()) {
       std::string baseName = "MonitorFile_Level";
@@ -1242,7 +1243,7 @@ void PeleC::writeMonitorFile(amrex::MultiFab& S, const amrex::Real *mwt, amrex::
 
       std::ofstream MonitorFile;
       MonitorFile.open(monitorFileName.c_str(), std::ios::out | std::ios::app);
-      MonitorFile << time << "\t" << min_nE  << "\t" << max_nE  << "\t" << min_nO4p  << "\t" << max_nO4p  << "\t" << min_nO2m  << "\t" << max_nO2m  << "\t" << min_phiV  << "\t" << max_phiV  << "\t" << min_EN  << "\t" << max_EN  << "\t" << disp_current << "\t" << Ddtodx2  << "\t" << CFL << "\t" << min_dielectric  <<"\t" << dt << std::endl;
+      MonitorFile << time << "\t" << min_nE  << "\t" << max_nE  << "\t" << min_nO4p  << "\t" << max_nO4p  << "\t" << min_nO2m  << "\t" << max_nO2m  << "\t" << min_phiV  << "\t" << max_phiV  << "\t" << min_EN  << "\t" << max_EN  << "\t" << disp_current << "\t" << Ddtodx2  << "\t" << CFL << "\t" << min_dielectric  << "\t" << min_edrift << "\t" << dt << std::endl;
       MonitorFile.close();
     }
   }
@@ -1279,7 +1280,7 @@ void PeleC::monitorFileSetup(int i){
     if(pac_mechanism){
       MonitorFile << "(1)time[s] \t (2)E/N[Td] \t (3)Te[eV] \t (4)Tgas[k] \t (5)nE[1/cm3] \t (6)nO[1/cm3] \t (7)nOH[1/cm3] \t (8)nH[1/cm3] \t (9)nN2*[1/cm3] \t (10)nCO[1/cm3] \t (11)nCO2[1/cm3] \t (12)nC2H4[1/cm3] \t (13)dt[s]" << std::endl;
     }else{
-      MonitorFile << "(1)time[s] \t (2)min_nE[1/cm3] \t (3)max_nE[1/cm3] \t (4)min_nO4+[1/cm3] \t (5)max_nO4+[1/cm3] \t (6)min_nO2-[1/cm3] \t (7)max_nO2-[1/cm3] \t (8)min_phiV[kV] \t (9)max_phiV[kV] \t (10)min_EN[Td] \t (11)max_EN[Td] \t (12)disp_current[C/s] \t (13)Ddtodx2 \t (14)CFL \t (15)min_dielectric[s] \t (16)dt[s]" << std::endl;
+      MonitorFile << "(1)time[s] \t (2)min_nE[1/cm3] \t (3)max_nE[1/cm3] \t (4)min_nO4+[1/cm3] \t (5)max_nO4+[1/cm3] \t (6)min_nO2-[1/cm3] \t (7)max_nO2-[1/cm3] \t (8)min_phiV[kV] \t (9)max_phiV[kV] \t (10)min_EN[Td] \t (11)max_EN[Td] \t (12)disp_current[C/s] \t (13)Ddtodx2 \t (14)CFL \t (15)min_dielectric[s] \t (16)min_drift[s] \t (17)dt[s]" << std::endl;
     }
     MonitorFile.close();
   }
